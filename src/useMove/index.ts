80270261cbab71e9f0fd38ca135f45beadb7cb1c
zhaoyuanmeng <1426593075@qq.com>
2022-09-02 10:00:28 +0800

    chore: 修改结构

--- src/useMove/index.ts
+++ src/useMove/index.ts
@@ -1,18 +1,13 @@
-// 引入包 自己手动引入的 
-// TODO:后期借助node自动从npm导入包 然后注入到dist文件里，方便cdn那种的使用
-// import { Arc, CanvasEngine, Rect,EventName } from "../../base/index.mjs";
 import { Arc, CanvasEngine, Rect,EventName } from "ycanvas";
-// 在包的基础上扩展
 
 import type {CanvasEngineProps} from '../types/useMove'
-// 导出包
 
 // 这个没有封装，思路是从base上继承然后添加新方法，最后组合成一个功能
 // 优化点：传参，能否公用engine
 export function useMove(option:CanvasEngineProps) {
   // 利用闭包的思路写
   let isDrag:boolean = false;
-  // 可以再这里面先生成一个canvas
+  // 可以再这里面先生成一个canvas,这里写死了 可以拿option里面的参数
   const engine2 = new CanvasEngine({
     w: "200",
     h: "200",
